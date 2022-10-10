8b87c55af9c99de221ffaa6b5944c5fdb59c403b
James Phillips <scorpi4n@scorpi4n.com>
2022-10-09 20:16:30 -0500

    Implement button functions and transitions

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const form = (function() {
+	let submitBtn = document.getElementById('submit')
+	
+	let form1 = document.getElementById('form-1')
+	let form2 = document.getElementById('form-2')
+	
+	function nextForm() {
+		form1.style.left = '-200vw'
+		form2.style.left = '15vw'
+	}
+	
+	function prevForm() {
+		form1.style.left = '15vw'
+		form2.style.left = '200vw'
+	}
+	
+	(function cacheBtns() {
+		let nextBtn = document.getElementById('next')
+		let backBtn = document.getElementById('back')
+		nextBtn.addEventListener('click', nextForm)
+		backBtn.addEventListener('click', prevForm)
+	})()
+
+	// return {
+	// 	nextForm,
+	// 	prevForm
+	// }
+})()
+
 const gameboard = (function() {
 	let board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 	let HTMLgameboard = document.querySelector('.gameboard')
