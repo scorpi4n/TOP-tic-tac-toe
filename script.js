b537f33d791296879647168a6b1b8aa56e3bb5b0
James Phillips <scorpi4n@scorpi4n.com>
2022-10-26 14:08:46 -0500

    Fix checkForWin return values

--- script.js
+++ script.js
@@ -113,7 +113,9 @@ const gameboard = (function() {
 		for (state of winStates) {
 			if (board[state[0]] == board[state[1]] && board[state[1]] == board[state[2]]) {
 				console.log(`${board[state[0]]} wins`)
-				return (board[state[1]] == 'o' ? false : true)
+				return board[state[1]] == 'o' ? -1 : 1
+			} else {
+				return 0
 			}
 		}
 	}
