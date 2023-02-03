8bb060ab079c3d186c74ce3c2f05f1a8943e6426
James Phillips <scorpi4n@scorpi4n.com>
2023-02-03 14:55:58 -0600

    Format with prettier

--- minimax.js
+++ minimax.js
@@ -1,63 +1,63 @@
 function evaluateGame(state) {
-	let value = 0
-	let winCheck = checkForWin(state)
+	let value = 0;
+	let winCheck = checkForWin(state);
 
 	if (winCheck == -1) {
-		value += 10
+		value += 10;
 		for (i of state) {
 			if (typeof i == typeof 0) {
-				value++
+				value++;
 			}
 		}
 	} else if (winCheck == 1) {
-		value -= 10
+		value -= 10;
 		for (i of state) {
 			if (typeof i == typeof 0) {
-				value--
+				value--;
 			}
 		}
 	}
 
-	return value
+	return value;
 }
 
 function getMoves(state) {
-	let moves = []
+	let moves = [];
 	for (i of state) {
-		if (typeof i != typeof '') {
-			let copyBoard = [...state]
-			copyBoard[state.indexOf(i)] = gameboard.getCurrentPlayer().getMarker()
-			moves.push(copyBoard)
-			console.log(copyBoard)
+		if (typeof i != typeof "") {
+			let copyBoard = [...state];
+			copyBoard[state.indexOf(i)] = gameboard.getCurrentPlayer().getMarker();
+			moves.push(copyBoard);
+			console.log(copyBoard);
 		}
 	}
-	return moves
+	return moves;
 }
 
 function minimax(gameState, depth, isMaximizing) {
 	if (depth == 0) {
 		// return static evaluation
-		return evaluateGame(gameState)
+		return evaluateGame(gameState);
 	}
 
-	let moves = getMoves(gameState)
+	let moves = getMoves(gameState);
 
 	// is maximizing player
 	if (isMaximizing) {
-		maxEval = -Infinity
-		gameboard.togglePlayer()
+		maxEval = -Infinity;
+		gameboard.togglePlayer();
 		for (move of moves) {
-			maxEval = Math.max(maxEval, minimax(move, depth - 1, false))
+			maxEval = Math.max(maxEval, minimax(move, depth - 1, false));
 		}
-		return maxEval
+		return maxEval;
 	}
 
 	// is NOT maximizing player
 	if (!isMaximizing) {
-		minEval = Infinity
+		minEval = Infinity;
 		for (move of moves) {
-			minEval = Math.min(minEval, minimax(move, depth - 1, true))
+			minEval = Math.min(minEval, minimax(move, depth - 1, true));
 		}
-		return minEval
+		return minEval;
 	}
 }
--- script.js
+++ script.js
@@ -1,120 +1,120 @@
-const p1 = playerFactory()
-const p2 = playerFactory()
+const p1 = playerFactory();
+const p2 = playerFactory();
 
 const form = (function () {
-	const xRadio = document.getElementById('X')
+	const xRadio = document.getElementById("X");
 	// const oRadio = document.getElementById('O')
-	const userNameInput = document.getElementById('name-1')
-	const checkbox = document.getElementById('ai')
-	const aiBanter = checkbox.nextElementSibling
-	const difficultyDropdown = document.getElementById('difficulty')
-	const enemyNameLabel = document.querySelector('label[for="name-2"]')
-	const enemyNameInput = document.getElementById('name-2')
-	const submitBtn = document.getElementById('submit')
+	const userNameInput = document.getElementById("name-1");
+	const checkbox = document.getElementById("ai");
+	const aiBanter = checkbox.nextElementSibling;
+	const difficultyDropdown = document.getElementById("difficulty");
+	const enemyNameLabel = document.querySelector('label[for="name-2"]');
+	const enemyNameInput = document.getElementById("name-2");
+	const submitBtn = document.getElementById("submit");
 
 	function addEventListeners() {
-		checkbox.addEventListener('input', toggleDifficultyInput)
-		submitBtn.addEventListener('click', function () {
-			p1.setMarker(xRadio.checked)
-			p1.setName(userNameInput.value)
+		checkbox.addEventListener("input", toggleDifficultyInput);
+		submitBtn.addEventListener("click", function () {
+			p1.setMarker(xRadio.checked);
+			p1.setName(userNameInput.value);
 
-			p2.setMarker(!xRadio.checked)
-			p2.setName(enemyNameInput.value)
+			p2.setMarker(!xRadio.checked);
+			p2.setName(enemyNameInput.value);
 
-			unrender()
-			gameboard.render()
-		})
+			unrender();
+			gameboard.render();
+		});
 	}
-	addEventListeners()
+	addEventListeners();
 
 	function removeEventListeners() {
-		checkbox.removeEventListener('input', toggleDifficultyInput)
-		submitBtn.removeEventListener('click', function () {
-			p2.setName(enemyNameInput.value)
-		})
+		checkbox.removeEventListener("input", toggleDifficultyInput);
+		submitBtn.removeEventListener("click", function () {
+			p2.setName(enemyNameInput.value);
+		});
 	}
 
 	function toggleDifficultyInput() {
-		let difficulty = document.getElementById('difficulty-setting')
+		let difficulty = document.getElementById("difficulty-setting");
 
 		if (checkbox.checked == true) {
-			difficulty.style.display = 'none'
-			aiBanter.style.display = 'none'
-			enemyNameLabel.style.display = 'block'
+			difficulty.style.display = "none";
+			aiBanter.style.display = "none";
+			enemyNameLabel.style.display = "block";
 		} else {
-			difficulty.style.display = 'block'
-			aiBanter.style.display = 'inline'
-			enemyNameLabel.style.display = 'none'
+			difficulty.style.display = "block";
+			aiBanter.style.display = "inline";
+			enemyNameLabel.style.display = "none";
 		}
 	}
 
 	function render() {
-		addEventListeners()
-		const form = document.querySelector('form')
-		form.style.display = 'flex'
+		addEventListeners();
+		const form = document.querySelector("form");
+		form.style.display = "flex";
 	}
 
 	function unrender() {
-		removeEventListeners()
-		const form = document.querySelector('form')
-		form.style.display = 'none'
+		removeEventListeners();
+		const form = document.querySelector("form");
+		form.style.display = "none";
 	}
 
 	return {
 		render,
-		unrender
-	}
-})()
+		unrender,
+	};
+})();
 
 const gameboard = (function () {
-	let board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-	const htmlGameboard = document.querySelector('.gameboard')
-	let currentPlayer = p1
+	let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+	const htmlGameboard = document.querySelector(".gameboard");
+	let currentPlayer = p1;
 
 	function togglePlayer() {
-		currentPlayer == p1 ? currentPlayer = p2 : currentPlayer = p1
+		currentPlayer == p1 ? (currentPlayer = p2) : (currentPlayer = p1);
 	}
 
 	function placeMarker(marker, index) {
-		let cells = document.querySelectorAll('.cell')
+		let cells = document.querySelectorAll(".cell");
 		for (i of cells) {
 			if (i.dataset.index == index) {
-				div = i
+				div = i;
 			}
 		}
 
-		if (typeof board[index] == typeof '') {
-			console.error('Somebody has already gone there.')
+		if (typeof board[index] == typeof "") {
+			console.error("Somebody has already gone there.");
 		} else {
-			div.classList.add(`active-${marker}`)
-			div.innerText = `${marker}`
-			board.splice(index, 1, marker)
+			div.classList.add(`active-${marker}`);
+			div.innerText = `${marker}`;
+			board.splice(index, 1, marker);
 		}
 	}
 
 	function render() {
-		htmlGameboard.style.display = 'grid'
+		htmlGameboard.style.display = "grid";
 		for (i of board) {
-			let div = document.createElement('div')
-			div.classList.add('cell', 'window', 'flex')
-			div.setAttribute('data-index', board.indexOf(i))
-			div.addEventListener('click', function () {
-				currentPlayer.playTurn(div.dataset.index)
-				togglePlayer()
-			})
-			htmlGameboard.appendChild(div)
+			let div = document.createElement("div");
+			div.classList.add("cell", "window", "flex");
+			div.setAttribute("data-index", board.indexOf(i));
+			div.addEventListener("click", function () {
+				currentPlayer.playTurn(div.dataset.index);
+				togglePlayer();
+			});
+			htmlGameboard.appendChild(div);
 		}
 	}
 
 	function unrender() {
 		while (htmlGameboard.lastChild) {
-			htmlGameboard.removeChild(htmlGameboard.lastChild)
+			htmlGameboard.removeChild(htmlGameboard.lastChild);
 		}
-		htmlGameboard.style.display = 'none'
+		htmlGameboard.style.display = "none";
 	}
 
 	function getCurrentPlayer() {
-		return currentPlayer
+		return currentPlayer;
 	}
 
 	return {
@@ -124,33 +124,33 @@ const gameboard = (function () {
 		// checkForWin,
 		render,
 		unrender,
-		getCurrentPlayer
-	}
-})()
+		getCurrentPlayer,
+	};
+})();
 
 function playerFactory() {
-	let name
-	let marker
+	let name;
+	let marker;
 
 	function setName(newName) {
-		name = newName
+		name = newName;
 	}
 
 	function getName() {
-		return name
+		return name;
 	}
 
 	function setMarker(bool) {
-		bool ? marker = 'x' : marker = 'o'
+		bool ? (marker = "x") : (marker = "o");
 	}
 
 	function getMarker() {
-		return marker
+		return marker;
 	}
 
 	function playTurn(index) {
-		gameboard.placeMarker(getMarker(), index)
-		checkForWin(gameboard.board)
+		gameboard.placeMarker(getMarker(), index);
+		checkForWin(gameboard.board);
 	}
 
 	return {
@@ -158,8 +158,8 @@ function playerFactory() {
 		getName,
 		setMarker,
 		getMarker,
-		playTurn
-	}
+		playTurn,
+	};
 }
 
 function checkForWin(gameState) {
@@ -177,14 +177,17 @@ function checkForWin(gameState) {
 		[2, 5, 8],
 		// diagonal wins
 		[0, 4, 8],
-		[2, 4, 6]
-	]
+		[2, 4, 6],
+	];
 
 	for (state of winStates) {
-		if (gameState[state[0]] == gameState[state[1]] && gameState[state[1]] == gameState[state[2]]) {
-			console.log(`${gameState[state[1]]} wins`)
-			return gameState[state[1]] == 'o' ? -1 : 1
+		if (
+			gameState[state[0]] == gameState[state[1]] &&
+			gameState[state[1]] == gameState[state[2]]
+		) {
+			console.log(`${gameState[state[1]]} wins`);
+			return gameState[state[1]] == "o" ? -1 : 1;
 		}
 	}
-	return 0
+	return 0;
 }
