45091501805cc742edb3455c0dbbf6ada23a160e
James Phillips <scorpi4n@scorpi4n.com>
2023-04-20 12:39:27 -0500

    refactor: move all game-logic functions into a single class that manages the state

--- src/game.ts
+++ src/game.ts
@@ -1,99 +1,114 @@
-import * as dom from "./dom.js";
+import * as dom from "./dom";
 
-class Player {
-  name;
-  marker;
+export class Player {
+  name: string;
+  private _marker: string;
 
-  constructor(name, marker) {
+  constructor(name: string, marker: string) {
     this.name = name;
-    this.marker = marker;
+    this._marker = marker.toLowerCase();
   }
 
-  playTurn(index) {
-    gameboard.placeMarker(getMarker(), index);
-    isTerminal(gameboard.board);
+  get marker() {
+    return this._marker;
   }
 }
 
 export class Game {
-  constructor() {}
-}
+  p1: Player;
+  p2: Player;
+  private _currentPlayer: Player;
+  private _board: (string | null)[];
 
-const board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-let currentPlayer = p1;
+  constructor(p1: Player, p2: Player) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this._currentPlayer = this.p1;
+    this._board = new Array(9).fill(null);
+  }
 
-function togglePlayer() {
-  currentPlayer == p1 ? (currentPlayer = p2) : (currentPlayer = p1);
-}
+  get currentPlayer() {
+    return this._currentPlayer;
+  }
 
-function placeMarker(marker, index) {
-  const cells = document.querySelectorAll(".cell");
-  for (i of cells) {
-    if (i.dataset.index == index) {
-      div = i;
+  get board() {
+    return this._board;
+  }
+
+  togglePlayer() {
+    this._currentPlayer == this.p1
+      ? (this._currentPlayer = this.p2)
+      : (this._currentPlayer = this.p1);
+  }
+
+  placeMarker(index: number) {
+    this.board[index] === null
+      ? (this.board[index] = this._currentPlayer.marker)
+      : console.log("Spot taken");
+
+    this.togglePlayer();
+  }
+
+  winner(): Player | null {
+    // 0, 1, 2,
+    // 3, 4, 5,
+    // 6, 7, 8
+
+    const terminalIndices = [
+      // horizontal wins
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // vertical wins
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // diagonal wins
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const index of terminalIndices) {
+      if (
+        this.board[index[0]] === this.board[index[1]] &&
+        this.board[index[1]] === this.board[index[2]] &&
+        this.board[index[1]] !== null
+      ) {
+        return this.p1.marker === this.board[index[1]] ? this.p1 : this.p2;
+      }
     }
+
+    return null;
   }
 
-  if (typeof board[index] == typeof "") {
-    console.error("Somebody has already gone there.");
-  } else {
-    div.classList.add(`active-${marker}`);
-    div.innerText = `${marker}`;
-    board.splice(index, 1, marker);
-  }
-}
+  isTerminal(): boolean {
+    if (this.winner() !== null) return true;
 
-function render() {
-  dom.gameboard.style.display = "grid";
-  for (i of board) {
-    const div = document.createElement("div");
-    div.classList.add("cell", "window", "flex");
-    div.setAttribute("data-index", board.indexOf(i));
-    div.addEventListener("click", function () {
-      currentPlayer.playTurn(div.dataset.index);
-      togglePlayer();
+    const isFull = !this.board.includes(null);
+    if (isFull) {
+      return true;
+    }
+
+    return false;
+  }
+
+  render() {
+    dom.gameboard.style.display = "grid";
+    this.board.forEach((_, i) => {
+      const div = document.createElement("div");
+      div.classList.add("cell", "window", "flex");
+      div.setAttribute("data-index", `${i}`);
+      div.addEventListener("click", () => {
+        this.placeMarker(parseInt(div.dataset.index || "6"));
+      });
+      dom.gameboard.appendChild(div);
     });
-    dom.gameboard.appendChild(div);
   }
-}
 
-function unrender() {
-  while (dom.gameboard.lastChild) {
-    dom.gameboard.removeChild(dom.gameboard.lastChild);
-  }
-  dom.gameboard.style.display = "none";
-}
-
-function getCurrentPlayer() {
-  return currentPlayer;
-}
-
-function isTerminal(gameState) {
-  // 0, 1, 2,
-  // 3, 4, 5,
-  // 6, 7, 8
-  const winStates = [
-    // horizontal wins
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    // vertical wins
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    // diagonal wins
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (state of winStates) {
-    if (
-      gameState[state[0]] == gameState[state[1]] &&
-      gameState[state[1]] == gameState[state[2]]
-    ) {
-      console.log(`${gameState[state[1]]} wins`);
-      return gameState[state[1]] == "o" ? -1 : 1;
+  unrender() {
+    while (dom.gameboard.lastChild) {
+      dom.gameboard.removeChild(dom.gameboard.lastChild);
     }
+    dom.gameboard.style.display = "none";
   }
-  return 0;
 }
