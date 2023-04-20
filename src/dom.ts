a6df1fccb760ffd6776a66334185071cf3367214
James Phillips <scorpi4n@scorpi4n.com>
2023-04-20 13:54:03 -0500

    refactor: move form element query selectors to dom.ts

--- src/dom.ts
+++ src/dom.ts
@@ -1,1 +1,24 @@
 export const gameboard = document.querySelector(".gameboard") as HTMLElement;
+
+// form
+export const form = document.querySelector("form") as HTMLFormElement;
+export const xRadio = document.getElementById("X") as HTMLFormElement;
+// export const oRadio = document.getElementById('O' )
+export const userNameInput = document.getElementById(
+  "name-1"
+) as HTMLFormElement;
+export const checkbox = document.getElementById("ai") as HTMLFormElement;
+export const aiBanter = checkbox?.nextElementSibling as HTMLFormElement;
+export const difficultyDropdown = document.getElementById(
+  "difficulty"
+) as HTMLFormElement;
+export const enemyNameLabel = document.querySelector(
+  'label[for="name-2"]'
+) as HTMLFormElement;
+export const enemyNameInput = document.getElementById(
+  "name-2"
+) as HTMLFormElement;
+export const difficulty = document.getElementById(
+  "difficulty-setting"
+) as HTMLFormElement;
+export const submitBtn = document.getElementById("submit") as HTMLFormElement;
